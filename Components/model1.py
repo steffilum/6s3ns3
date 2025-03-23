3d16bacfefbad9ab163bfc8f44d51741567a9ea6
Josiah Lee <josiahlee239@gmail.com>
2025-03-23 21:23:35 +0800

    new model based on c and ne

--- Components/model1.py
+++ Components/model1.py
@@ -4,7 +4,28 @@ from nett_exports import *
 fred = Fred(api_key = os.getenv("API_KEY"))
 
 df = get_most_recent_df_of_date("GDP", "2020-01-01", fred)
+df = pct_chg(df)
 
-print(quart_pct_chg_ne())
-print(quart_pct_chg_pce())
-print(df)
+
+# print(quart_pct_chg_ne())
+# print(quart_pct_chg_pce())
+# print(df)
+
+ne = quart_pct_chg_ne()
+pce = quart_pct_chg_pce()
+
+compiled = pd.concat([ne, pce, df.pct_chg], axis = 1).dropna()
+compiled.columns = ['Nett_Exports', 'PCE', 'GDP']
+
+X = compiled.iloc[:, :2]
+y = compiled.GDP
+
+X = sm.add_constant(X)
+model = sm.OLS(y, X).fit()
+print(model.summary())
+
+new_X = np.array([1, ne.values[-1], pce.values[-1]])
+
+predictions = model.predict(new_X)
+
+print("Predicted values:", predictions)
--- Components/package_imports/imports.py
+++ Components/package_imports/imports.py
@@ -7,6 +7,8 @@ from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.ar_model import AutoReg
 from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from sklearn.linear_model import LinearRegression
 from dotenv import load_dotenv
 import os
 from arch import arch_model
+import statsmodels.api as sm
