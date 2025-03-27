a0387c4e2ec6a6681a9b76079175114d53fd231b
Jason <jasonlow38@gmail.com>
2025-03-28 01:55:34 +0800

    add further features (forecast value) onto homepage

--- frontend/pages/homepage.py
+++ frontend/pages/homepage.py
@@ -4,6 +4,7 @@ import dash_bootstrap_components as dbc
 import os
 from pages.news import df_articles, generate_card_scroll
 from shared.default_pagelayout import get_default_layout 
+from data.fakedata1 import get_gdp_growth_rate, gdp_growth_df
 
 
 
@@ -25,7 +26,8 @@ homepage_content = html.Div(
         "zIndex": "1"
     },
     children=[
-        html.H3(
+        # Header "Top Stories this Week"
+        html.H1(
             "Top Stories this Week",
             style={
                 "color": "white",
@@ -35,6 +37,7 @@ homepage_content = html.Div(
                 "fontFamily": "Montserrat, sans-serif"
             }
         ),
+        # Container for the articles
         html.Div(
             children=[
                 generate_card_scroll(df_articles)
@@ -46,13 +49,75 @@ homepage_content = html.Div(
                 "overflowY": "auto",
                 "overflowX": "hidden"
             }
+        ),
+        html.Label(
+            "Select a Quarter to Forecast GDP Growth Rate",
+            style={
+                "color": "white",
+                "fontWeight": "600",
+                "fontSize": "22px",
+                "marginBottom": "20px",
+                "fontFamily": "Montserrat, sans-serif", 
+                "position": "absolute",
+                "left": "600px"
+            }
+        ),
+        # Range input
+        dcc.Input(
+            id = 'quarter-picker',
+            type = 'text',
+            placeholder='Enter Quarter (e.g. 1950Q1)',
+            value='1950Q1',  # Default value
+            style={"width": "150px", "height": "40px", "position": "absolute", "left": "800px"}
+        ),
+       
+        # Container for Forecast label and value
+        html.Div(
+            children = [
+                html.H1(
+                    "GDP Forecast for Next Quarter:",
+                    style={
+                        "color": "rgba(206, 203, 203)",
+                        "fontWeight": "600",
+                        "fontSize": "22px",
+                        "fontFamily": "Montserrat, sans-serif"
+                    }
+                ),
+                html.H2(
+                    id = 'gdp-forecast', children="",
+                    style={
+                        "color": "white",
+                        "fontWeight": "600",
+                        "fontSize": "28px",
+                        "fontFamily": "Montserrat, sans-serif"
+                    }
+                ),
+
+                ], style={"position": "absolute",
+                        "height": "5px",
+                        "width": "500px", 
+                        "left": "350px",
+                        "top": "-2px"}
         )
+        
+        
     ]
 )
 
 # Plug that content into your default layout
 layout = get_default_layout(main_content=homepage_content)
 
+# Callback to update the GDP forecast value
+@dash.callback(
+    Output('gdp-forecast', 'children'),
+    Input('quarter-picker', 'value')
+)
+
+def update_gdp_forecast(selected_quater):
+    if selected_quater not in gdp_growth_df['Quarter'].values:
+        return "No data found"
+    value = get_gdp_growth_rate(selected_quater)
+    return f"{value:.2f}%"
 
 
 
