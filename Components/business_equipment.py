05cbd944853ffc86173a1cc71e7b6228f166f756
QuekHongRui <quek_hong_rui@u.nus.edu>
2025-03-28 16:37:55 +0800

    Committing changes to investment proxies

--- Components/business_equipment.py
+++ Components/business_equipment.py
@@ -2,31 +2,42 @@ from package_imports import *
 
 fred = Fred(api_key = os.getenv("API_KEY"))
 
-df = get_most_recent_series_of_date("IPBUSEQ", "2020-01-01", fred)
+given_date = "2020-01-01"
+df = get_most_recent_series_of_date("IPBUSEQ", given_date, fred)
+df = df[df.index<=pd.to_datetime("2006-06-01")]
 
 pct_chg_business_equipment = transform_series(df, 5).dropna()
-
-# print("ADF Test Result: ", adfuller(pct_chg_business_equipment))
-# plot_acf_pacf(pct_chg_pce['demean_pct_chg'] )
-# plot_acf_pacf(pct_chg_pce['demean_pct_chg']**2)
+# pct_chg_business_equipment.plot()
 # plt.show()
 
-#looks like a garch(1, 1) is suitable
-model = arch_model(pct_chg_business_equipment, mean = 'AR', lags = 1, vol='Garch', p=1, q=1)
-fit = model.fit()
-# print(fit.summary())
+pct_chg_business_equipment = pct_chg_business_equipment.diff().dropna()
+# pct_chg_business_equipment.plot()
+# plt.show()
 
-last_month = pct_chg_business_equipment.index[-1]+ pd.offsets.MonthBegin(1)
+# print("ADF Test Result: ", adfuller(pct_chg_business_equipment)) # small p-value, stationary 
 
-#prediction
-pred = fit.forecast(horizon=4-int(last_month.month)%4).mean.iloc[-1].values
+# plot_acf_pacf(pct_chg_business_equipment)
+# plt.show() # guess p = 5, q = 1
 
-new_dates = pd.date_range(start = last_month , periods = 4-int(last_month.month)%4, freq='MS')
-new_df = pd.Series(pred, index=new_dates)
+best_p, best_q = best_arma(pct_chg_business_equipment, max_p = 5, max_q = 1)
+print(best_p, best_q)
 
-pct_chg_pred = pd.concat([pct_chg_business_equipment, new_df])
+# #looks like a garch(1, 1) is suitable
+# model = arch_model(pct_chg_business_equipment, mean = 'AR', lags = 1, vol='Garch', p=1, q=1)
+# fit = model.fit()
+# # print(fit.summary())
 
-quarterly_pct_chage = pct_chg_pred.resample('QS').sum()
+# last_month = pct_chg_business_equipment.index[-1]+ pd.offsets.MonthBegin(1)
 
-def quart_pct_chg_pce(date = "2020-01-01"):
-    return quarterly_pct_chage
+# #prediction
+# pred = fit.forecast(horizon=4-int(last_month.month)%4).mean.iloc[-1].values
+
+# new_dates = pd.date_range(start = last_month , periods = 4-int(last_month.month)%4, freq='MS')
+# new_df = pd.Series(pred, index=new_dates)
+
+# pct_chg_pred = pd.concat([pct_chg_business_equipment, new_df])
+
+# quarterly_pct_chage = pct_chg_pred.resample('QS').sum()
+
+# def quart_pct_chg_pce(date = "2020-01-01"):
+#     return quarterly_pct_chage
--- Components/business_inventories.py
+++ Components/business_inventories.py
@@ -2,38 +2,44 @@ from package_imports import *
 
 fred = Fred(api_key = os.getenv("API_KEY"))
 
-df = get_most_recent_series_of_date("BUSINV", "2020-01-01", fred)
+given_date = "2006-12-01"
+
+df = get_most_recent_series_of_date("BUSINV", given_date, fred)
+df = df[df.index<=pd.to_datetime("2006-06-01")]
+print(df)
 
 pct_chg_business_inventories = transform_series(df, 5).dropna()
+pct_chg_business_inventories.plot()
+plt.show()
 
 # pct_chg_business_inventories.plot()
 # plt.show() ## two years of outliers observed, in ~ 2002 and ~ 2009
 
-pct_chg_business_inventories = pct_chg_business_inventories.to_frame().rename(columns = {0: "value"})
-pct_chg_business_inventories["OutlierIndicator"] = np.where(pct_chg_business_inventories["value"] <= -0.01, 1, 0) ## set a dummy variable to mark outliers
+# pct_chg_business_inventories = pct_chg_business_inventories.to_frame().rename(columns = {0: "value"})
+# pct_chg_business_inventories["OutlierIndicator"] = np.where(pct_chg_business_inventories["value"] <= -0.01, 1, 0) ## set a dummy variable to mark outliers
 
-## checking for stationarity -- small p-value: stationary
-# print("ADF Test Result: ", adfuller(pct_chg_business_inventories["value"]))
+# ## checking for stationarity -- small p-value: stationary
+# # print("ADF Test Result: ", adfuller(pct_chg_business_inventories["value"]))
 
-# plot_acf_pacf(pct_chg_business_inventories["value"]) ## guess p as 3, q as 8
-# plt.show()
+# # plot_acf_pacf(pct_chg_business_inventories["value"]) ## guess p as 3, q as 8
+# # plt.show()
 
-# #looks like a garch(1, 1) is suitable
-# model = arch_model(pct_chg_business_inventories, mean = 'AR', lags = 1, vol='Garch', p=1, q=1)
-# fit = model.fit()
-# # # print(fit.summary())
+# # #looks like a garch(1, 1) is suitable
+# # model = arch_model(pct_chg_business_inventories, mean = 'AR', lags = 1, vol='Garch', p=1, q=1)
+# # fit = model.fit()
+# # # # print(fit.summary())
 
-# last_month = pct_chg_business_inventories.index[-1]+ pd.offsets.MonthBegin(1)
+# # last_month = pct_chg_business_inventories.index[-1]+ pd.offsets.MonthBegin(1)
 
-# #prediction
-# pred = fit.forecast(horizon=4-int(last_month.month)%4).mean.iloc[-1].values
+# # #prediction
+# # pred = fit.forecast(horizon=4-int(last_month.month)%4).mean.iloc[-1].values
 
-# new_dates = pd.date_range(start = last_month , periods = 4-int(last_month.month)%4, freq='MS')
-# new_df = pd.Series(pred, index=new_dates)
+# # new_dates = pd.date_range(start = last_month , periods = 4-int(last_month.month)%4, freq='MS')
+# # new_df = pd.Series(pred, index=new_dates)
 
-# pct_chg_pred = pd.concat([pct_chg_business_inventories, new_df])
+# # pct_chg_pred = pd.concat([pct_chg_business_inventories, new_df])
 
-# quarterly_pct_chage = pct_chg_pred.resample('QS').sum()
+# # quarterly_pct_chage = pct_chg_pred.resample('QS').sum()
 
-# def quart_pct_chg_pce(date = "2020-01-01"):
-#      return quarterly_pct_chage
+# # def quart_pct_chg_pce(date = "2020-01-01"):
+# #      return quarterly_pct_chage
