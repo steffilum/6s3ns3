6240f9f3a8a023e227d0ecf9e8cb2b3e70872b8c
Josiah Lee <josiahlee239@gmail.com>
2025-03-28 16:14:39 +0800

    Redo C

--- Components/consumption.py
+++ Components/consumption.py
@@ -2,43 +2,59 @@ from package_imports import *
 
 fred = Fred(api_key = os.getenv("API_KEY"))
 
-given_date = "2020-01-01"
+given_date = "2006-12-01"
 
 df = get_most_recent_series_of_date("PCE", given_date, fred)
+df = df[df.index<=pd.to_datetime("2006-06-01")]
 
 pct_chg_pce = transform_series(df, 5).dropna()*100
 # pct_chg_pce.plot()
 # plt.show()
-# Visually the data looks like it was going up then drops after 1975
-pct_chg_pce = pct_chg_pce[pct_chg_pce.index>pd.to_datetime("1975-01-01")]
-# pct_chg_pce.plot()
-# plt.show()
+
 # checking for stationarity
-print("ADF Test Result: ", adfuller(pct_chg_pce, regression='c'))
+# print("ADF Test Result: ", adfuller(pct_chg_pce, regression='c'))
 # plot_acf_pacf(pct_chg_pce)
 # plt.show()
 
 #gridsearch chosen base on pcf and acf
-best_arma(pct_chg_pce, trend='c', test_size=10, start_p= 0, start_q=0, max_p=5, max_q=5)
-model = ARIMA(pct_chg_pce, order=(0, 0, 0), trend = 'c', freq = 'MS').fit()
-fig, ax = plt.subplots()
-ax.plot(model.fittedvalues)
-ax.plot(pct_chg_pce)
+#seasonal order based on acf
+# best_arma(pct_chg_pce, trend='c', test_size=10, start_p= 0, start_q=0, max_p=5, max_q=5, seasonal_order = (0, 0, 0, 6))
+model = ARIMA(pct_chg_pce, order=(4, 0, 3), trend = 'c', freq = 'MS', seasonal_order=(0, 0, 0, 6))
+model = model.fit(start_params = np.full(4+3+6+1, .01))
+
+# fig, ax = plt.subplots()
+# ax.plot(model.fittedvalues)
+# ax.plot(pct_chg_pce)
+# plt.show()
+# plot_acf_pacf(model.resid)
+plt.plot(model.resid)
 plt.show()
-# pred = model.get_forecast(steps = 1).predicted_mean
-# print(pred)
+
 
 start_date_pred = pct_chg_pce.index[-1]+ pd.offsets.MonthBegin(1)
-end_date_pred = pd.Period(given_date, freq='Q').start_time + pd.offsets.MonthBegin(-1)
+end_date_pred = pd.Period(given_date, freq='Q').end_time.to_period(freq='M').start_time
 
 #prediction
-pred = model.predict(horizon=4-int(last_month.month)%4).mean.iloc[-1].values
+pred = model.predict(start = start_date_pred, end = end_date_pred)
 
-new_dates = pd.date_range(start = last_month , periods = 4-int(last_month.month)%4, freq='MS')
-new_df = pd.Series(pred, index=new_dates)
-pct_chg_pred = pd.concat([pct_chg_pce['pct_chg'], new_df])
+pct_chg_pred = pd.concat([pct_chg_pce, pred])
 
 quarterly_pct_chage = pct_chg_pred.resample('QS').sum()
 
-def quart_pct_chg_pce(date = "2020-01-01"):
-    return quarterly_pct_chage
+# takes in the given dates and return values up to the date if have if not predict
+#takes in given date and period, so 'Q' or 'M' for bridge or midas
+def quart_pct_chg_pce(given_date = "2020-01-01", period = 'Q'):
+    fred = Fred(api_key = os.getenv("API_KEY"))
+    df = get_most_recent_series_of_date("PCE", given_date, fred)
+    pct_chg_pce = transform_series(df, 5).dropna()*100
+    model = ARIMA(pct_chg_pce, order=(4, 0, 3), trend = 'c', freq = 'MS').fit()
+    start_date_pred = pct_chg_pce.index[-1]+ pd.offsets.MonthBegin(1)
+    end_date_pred = pd.Period(given_date, freq='Q').end_time.to_period(freq='M').start_time
+    pred = model.predict(start = start_date_pred, end = end_date_pred)
+    pct_chg_pred = pd.concat([pct_chg_pce, pred])
+    if period == 'M':
+        return pct_chg_pred
+    elif period == 'Q':
+
+        quarterly_pct_chage = pct_chg_pred.resample('QS').sum()
+        return quarterly_pct_chage
