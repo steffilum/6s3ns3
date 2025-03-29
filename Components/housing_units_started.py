4c7f0580f081036077bfa3347fce75ac4f9d6870
QuekHongRui <quek_hong_rui@u.nus.edu>
2025-03-29 16:42:22 +0800

    Committing housing_units_started.py

--- Components/housing_units_started.py
+++ Components/housing_units_started.py
@@ -2,30 +2,58 @@ from package_imports import *
 
 fred = Fred(api_key = os.getenv("API_KEY"))
 
-df = get_most_recent_series_of_date("HOUST", "2020-01-01", fred)
+get_date = "2006-12-01"
+df = get_most_recent_series_of_date("HOUST", get_date, fred)
+df = df[df.index<=pd.to_datetime("2006-06-01")]
 
 pct_chg_housing_units_started = transform_series(df, 4)
-
-# print("ADF Test Result: ", adfuller(pct_chg_housing_units_started))
-# plot_acf_pacf(pct_chg_pce['demean_pct_chg'] )
-# plot_acf_pacf(pct_chg_pce['demean_pct_chg']**2)
+# pct_chg_housing_units_started.plot()
+# plt.show()
+pct_chg_housing_units_started = pct_chg_housing_units_started.diff().dropna() ## recommended transformation seems to be inadequate in making the data series stationary, so additional differencing required
+# pct_chg_housing_units_started.plot()
 # plt.show()
 
-#looks like a garch(1, 1) is suitable
-model = arch_model(pct_chg_housing_units_started, mean = 'AR', lags = 1, vol='Garch', p=1, q=1)
-fit = model.fit()
-# print(fit.summary())
+# print("ADF Test Result: ", adfuller(pct_chg_housing_units_started))
+# plot_acf_pacf(pct_chg_housing_units_started )
+# plt.show() ## guess p = 1, q = 1 for ARIMA model
 
-last_month = pct_chg_housing_units_started.index[-1]+ pd.offsets.MonthBegin(1)
+# best_p, best_q = best_arma(pct_chg_housing_units_started, max_p = 1, max_q = 1, trend = "n", test_size = 10) ## best p and q are 1 and 1 respectively
+model = ARIMA(pct_chg_housing_units_started, order=(1, 0, 1), trend = 'n', freq = 'MS')
+model = model.fit(start_params = np.full(1 + 1 + 1, .01))
+
+# fig, ax = plt.subplots()
+# ax.plot(model.fittedvalues, label = "fitted")
+# ax.plot(pct_chg_housing_units_started, label = "actual")
+# ax.legend(loc="upper left")
+# plt.show()
+
+# plot_acf_pacf(model.resid)
+# plt.plot(model.resid)
+# plt.show()
+
+start_date_pred = pct_chg_housing_units_started.index[-1]+ pd.offsets.MonthBegin(1)
+end_date_pred = pd.Period(pct_chg_housing_units_started, freq='Q').end_time.to_period(freq='M').start_time
 
 #prediction
-pred = fit.forecast(horizon=4-int(last_month.month)%4).mean.iloc[-1].values
+pred = model.predict(start = start_date_pred, end = end_date_pred)
 
-new_dates = pd.date_range(start = last_month , periods = 4-int(last_month.month)%4, freq='MS')
-new_df = pd.Series(pred, index=new_dates)
-pct_chg_pred = pd.concat([pct_chg_housing_units_started, new_df])
+pct_chg_pred = pd.concat([pct_chg_housing_units_started, pred])
 
 quarterly_pct_chage = pct_chg_pred.resample('QS').sum()
 
-def quart_pct_chg_pce(date = "2020-01-01"):
-    return quarterly_pct_chage
+def quart_pct_chg_housing_units_started(given_date = "2020-01-01", period = 'Q'):
+    fred = Fred(api_key = os.getenv("API_KEY"))
+    df = get_most_recent_series_of_date("HOUST", given_date, fred)
+    df = df[df.index<pd.Timestamp(given_date).to_period('M').start_time - pd.offsets.MonthBegin(1)]
+    pct_chg_housing_units_started = transform_series(df, 4).diff().dropna()*100
+    model = ARIMA(pct_chg_housing_units_started, order=(1, 0, 1), trend = 'n', freq = 'MS').fit(start_params = np.full(1+1+1, .01))
+    start_date_pred = pct_chg_housing_units_started.index[-1]+ pd.offsets.MonthBegin(1)
+    end_date_pred = pd.Period(given_date, freq='Q').end_time.to_period(freq='M').start_time
+    pred = model.predict(start = start_date_pred, end = end_date_pred)
+    pct_chg_pred = pd.concat([pct_chg_housing_units_started, pred])
+    if period == 'M':
+        return pct_chg_pred
+    elif period == 'Q':
+
+        quarterly_pct_chage = pct_chg_pred.resample('QS').sum()
+        return quarterly_pct_chage
