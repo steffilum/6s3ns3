11e48f88bf7b1d9bdbe449fc5a52549712bd7098
Josiah Lee <josiahlee239@gmail.com>
2025-04-04 16:55:54 +0800

    Test for LSA assumptions
    Empirically, LSA assumptions of independence btw regressors and error terms seem to hold

--- Components/model1.py
+++ Components/model1.py
@@ -11,7 +11,7 @@ df = df.pct_chg
 
 _, test = train_test_split(df, test_size=50, shuffle=False)
 
-X, y = load_data_bridge_nohouse(given_date=given_date)
+X, y = load_data_bridge(given_date=given_date)
 # X, _, y, _ = train_test_split(X.iloc[:-1, :], y, test_size=50, shuffle=False)
 
 # vif_data = pd.DataFrame()
@@ -29,28 +29,30 @@ X, y = load_data_bridge_nohouse(given_date=given_date)
 # condition_number = np.linalg.cond(X.values)
 # print(f"Condition number: {condition_number}")
 
-# Evaluation on test set
-pred = []
-for index in range(1, 51):
-    date = pd.to_datetime(given_date)
-    new_date = date - pd.DateOffset(months=3*index)
-    new_date_str = new_date.strftime('%Y-%m-%d')
-    with open(f'Components/test_data_bridge_nohouse/data_iteration_{new_date_str}.pkl', 'rb') as f:
-        X_train, y_train = pickle.load(f)
-    X_train = sm.add_constant(X_train)    
-    X_test = X_train.iloc[-1, :]
-    X_train = X_train.iloc[:-1, :]
-    model = sm.OLS(y_train, X_train).fit()
-    pred.append(model.predict(X_test)[0])
-    print(f"Iteration {index}")
+# # Evaluation on test set
+# pred = []
+# for index in range(1, 51):
+#     date = pd.to_datetime(given_date)
+#     new_date = date - pd.DateOffset(months=3*index)
+#     new_date_str = new_date.strftime('%Y-%m-%d')
+#     with open(f'Components/test_data_bridge_nohouse/data_iteration_{new_date_str}.pkl', 'rb') as f:
+#         X_train, y_train = pickle.load(f)
+#     X_train = sm.add_constant(X_train)    
+#     X_test = X_train.iloc[-1, :]
+#     X_train = X_train.iloc[:-1, :]
+#     model = sm.OLS(y_train, X_train).fit()
+#     pred.append(model.predict(X_test)[0])
+#     print(f"Iteration {index}")
 
-pred.reverse()
+# pred.reverse()
 
-pred = pd.Series(pred, index = test.index)
+# pred = pd.Series(pred, index = test.index)
 
-#evaluation
-eval(pred, test)
+# #evaluation
+# eval(pred, test)
 
 model = sm.OLS(y, X.iloc[:-1, :]).fit()
 print(model.summary())
 print(model.predict(X.iloc[:-1, :])[0])
+residuals = model.resid
+print(X.iloc[:-1, :].apply(lambda x: x.corr(residuals)))
