11fd51ee286e9b7c6da94bcfe9c229f18f6ff995
QuekHongRui <quek_hong_rui@u.nus.edu>
2025-04-07 16:26:54 +0800

    Using most updated Components folder

--- Components/arft04.py
+++ Components/arft04.py
@@ -2,7 +2,9 @@ from package_imports import *
 
 fred = Fred(api_key = os.getenv("API_KEY"))
 
-df = get_most_recent_series_of_date("GDP", "2020-01-01", fred)
+given_date = "2020-03-01"
+
+df = get_most_recent_series_of_date("GDP", given_date, fred)
 df = pct_chg(df)
 
 df = df.pct_chg
@@ -11,20 +13,28 @@ _, test = train_test_split(df, test_size=50, shuffle=False)
 
 df.index = pd.to_datetime(df.index).to_period('Q')
 
-# print(df.shape)
+print(df.shape)
 
 pred = []
 
 for index in range(1, 51):
-    train = df.iloc[:290-index-1]
+    date = pd.to_datetime(given_date)
+    new_date = date - pd.DateOffset(months=3*index)
+    new_date_str = new_date.strftime('%Y-%m-%d')
+    with open(f'Components/test_data_bridge/data_iteration_{new_date_str}.pkl', 'rb') as f:
+        _, train = pickle.load(f)   
+    train.index = pd.to_datetime(train.index).to_period('Q')
     model = AutoReg(train, lags = 4, trend = 'ct').fit()
-    pred.append(model.predict(start = 290-index, end = 290-index))
+    pred.append(model.predict(start = len(train), end = len(train)))
 
 # print(model.params)
 
 pred.reverse()
+print(pred)
 pred = pd.concat(pred)
 pred.index = pred.index.to_timestamp()
+print(pred)
+print(test)
 
 eval(test, pred, plot=True)
 
--- Components/model1.py
+++ Components/model1.py
@@ -1,29 +1,56 @@
 from data_load import *
 
-X, y = load_data()
+given_date = "2020-03-01"
 
-X = sm.add_constant(X)
-# print(X.shape)
+fred = Fred(api_key = os.getenv("API_KEY"))
+
+df = get_most_recent_series_of_date("GDP", given_date, fred)
+df = pct_chg(df)
+
+df = df.pct_chg
+
+_, test = train_test_split(df, test_size=50, shuffle=False)
+
+X, y = load_data_bridge_nohouse(given_date=given_date)
+# X, _, y, _ = train_test_split(X.iloc[:-1, :], y, test_size=50, shuffle=False)
+
+# vif_data = pd.DataFrame()
+# vif_data["Feature"] = X.columns
+# vif_data["VIF"] = [variance_inflation_factor(X.values, i) for i in range(X.shape[1])]
+# print(vif_data)
+# # suggest to remove housing start
+
+# # Even though govt construction is not correlated w GDP, when adding model performance is better and condition number is similar so include
+# combined = pd.concat([X, y], axis = 1)
+# corr_matrix = combined.corr()
+# sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
+# plt.show()
+
+# condition_number = np.linalg.cond(X.values)
+# print(f"Condition number: {condition_number}")
 
 # Evaluation on test set
 pred = []
 for index in range(1, 51):
-    X_train = X.iloc[:-index, :]
-    y_train = y.iloc[:-index]
-    X_test = X.iloc[-index, :]
+    date = pd.to_datetime(given_date)
+    new_date = date - pd.DateOffset(months=3*index)
+    new_date_str = new_date.strftime('%Y-%m-%d')
+    with open(f'Components/test_data_bridge_nohouse/data_iteration_{new_date_str}.pkl', 'rb') as f:
+        X_train, y_train = pickle.load(f)
+    X_train = sm.add_constant(X_train)    
+    X_test = X_train.iloc[-1, :]
+    X_train = X_train.iloc[:-1, :]
     model = sm.OLS(y_train, X_train).fit()
     pred.append(model.predict(X_test)[0])
+    print(f"Iteration {index}")
 
 pred.reverse()
 
-y_test = y.iloc[-50:]
-pred = pd.Series(pred, index = y_test.index)
+pred = pd.Series(pred, index = test.index)
 
 #evaluation
-eval(pred, y_test)
+eval(pred, test)
 
-# new_X = np.array([1, ne.values[-1], pce.values[-1], df.lag_GDP[-1]])
-
-# predictions = model.predict(new_X)
-
-# print("Predicted values:", predictions)
+model = sm.OLS(y, X.iloc[:-1, :]).fit()
+print(model.summary())
+print(model.predict(X.iloc[:-1, :])[0])
