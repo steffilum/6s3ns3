ffb7c303c6cd7e44e71f25b781a016cb1ae2f16e
Josiah Lee <josiahlee239@gmail.com>
2025-04-09 17:00:58 +0800

    Update dm for demo

--- Components/dm.py
+++ Components/dm.py
@@ -3,8 +3,7 @@ from package_imports import *
 
 test = pd.read_csv('Components/Predictions/test.csv', index_col=0)
 pred1 = pd.read_csv('Components/Predictions/rf_midas.csv', index_col=0)
-pred2 = pd.read_csv('Components/Predictions/rf_bridge.csv', index_col=0)
-print(pred1)
+pred2 = pd.read_csv('Components/Predictions/benchmark1.csv', index_col=0)
 stat, p, se, mean = dm_test(test, pred1, pred2)
 print(f"DM stat: {stat}")
 print(f"p-value: {p}")
