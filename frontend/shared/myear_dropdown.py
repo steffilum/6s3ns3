db8ec9778bd64b63ecb5761830847e7d09878677
steffilum <113579847+steffilum@users.noreply.github.com>
2025-04-10 13:26:40 +0800

    Add clearable = false for dropdown

--- frontend/shared/myear_dropdown.py
+++ frontend/shared/myear_dropdown.py
@@ -17,12 +17,14 @@ def myear_dropdown():
                     options=[{'label': m, 'value': m} for m in ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
                                                                 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']],
                     value= calendar.month_abbr[datetime.now().month],
+                    clearable = False,
                     className="myear-dropdown"
                 ),
                 dcc.Dropdown(
                     id='year-dropdown',
                     options=[{'label': str(year), 'value': str(year)} for year in range(datetime.now().year, 1999, -1)],
                     value=str(datetime.now().year),
+                    clearable = False,
                     className="myear-dropdown"
                 ),
     ]
